6e0f559b25c915e4961484e9f33f03fffa8c4138
Matthias Endler <matthias-endler@gmx.net>
2020-08-09 23:12:25 +0200

    Switch to linkify to cover non MD links

--- src/extract.rs
+++ src/extract.rs
@@ -1,19 +1,16 @@
-use pulldown_cmark::{Event, Parser, Tag};
+use linkify::{LinkFinder, LinkKind};
+
 use std::collections::HashSet;
 use url::Url;
 
-pub(crate) fn extract_links(md: &str) -> HashSet<Url> {
-    let mut links: Vec<String> = Vec::new();
-    Parser::new(md).for_each(|event| match event {
-        Event::Start(Tag::Link(_, link, _)) => links.push(link.into_string()),
-        Event::Start(Tag::Image(_, link, _)) => links.push(link.into_string()),
-        _ => (),
-    });
+pub(crate) fn extract_links(input: &str) -> HashSet<Url> {
+    let finder = LinkFinder::new();
+    let links: Vec<_> = finder.links(input).collect();
 
     // Only keep legit URLs. This sorts out things like anchors.
     // Silently ignore the parse failures for now.
     // TODO: Log errors in verbose mode
-    let links: HashSet<Url> = links.iter().flat_map(|l| Url::parse(&l)).collect();
+    let links: HashSet<Url> = links.iter().flat_map(|l| Url::parse(l.as_str())).collect();
     debug!("Testing links: {:#?}", links);
 
     links
