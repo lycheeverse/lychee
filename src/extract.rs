c6b6f5a67fb94f7e783640dcff7bc75a72a530ed
Paweł Romanowski <pawroman@pawroman.dev>
2021-01-06 14:35:52 +0100

    Use html5ever for HTML link extraction

--- src/extract.rs
+++ src/extract.rs
@@ -1,8 +1,10 @@
 use crate::collector::InputContent;
 use crate::uri::Uri;
+use html5ever::parse_document;
+use html5ever::tendril::{StrTendril, TendrilSink};
 use linkify::LinkFinder;
+use markup5ever_rcdom::{Handle, NodeData, RcDom};
 use pulldown_cmark::{Event as MDEvent, Parser, Tag};
-use quick_xml::{events::Event as HTMLEvent, Reader};
 use std::collections::HashSet;
 use std::path::Path;
 use url::Url;
@@ -35,13 +37,13 @@ impl<P: AsRef<Path>> From<P> for FileType {
     }
 }
 
-// Use LinkFinder here to offload the actual link searching
+// Use LinkFinder here to offload the actual link searching in plaintext.
 fn find_links(input: &str) -> Vec<linkify::Link> {
     let finder = LinkFinder::new();
     finder.links(input).collect()
 }
 
-// Extracting unparsed URL strings from a markdown string
+/// Extract unparsed URL strings from a markdown string.
 fn extract_links_from_markdown(input: &str) -> Vec<String> {
     let parser = Parser::new(input);
     parser
@@ -57,75 +59,94 @@ fn extract_links_from_markdown(input: &str) -> Vec<String> {
         .collect()
 }
 
-// Extracting unparsed URL strings from a HTML string
+/// Extract unparsed URL strings from a HTML string.
 fn extract_links_from_html(input: &str) -> Vec<String> {
-    let mut reader = Reader::from_str(input);
+    let tendril = StrTendril::from(input);
+    let rc_dom = parse_document(RcDom::default(), Default::default()).one(tendril);
 
-    // allow not well-formed XML documents, which contain non-closed elements
-    // (e.g. HTML5 which has things like `<link>`)
-    reader.check_end_names(false);
-
-    let mut buf = Vec::new();
     let mut urls = Vec::new();
 
-    while let Ok(e) = reader.read_event(&mut buf) {
-        match e {
-            HTMLEvent::Start(ref e) | HTMLEvent::Empty(ref e) => {
-                for attr in e.attributes() {
-                    if let Ok(attr) = attr {
-                        match (attr.key, e.name()) {
-                            (b"href", b"a")
-                            | (b"href", b"area")
-                            | (b"href", b"base")
-                            | (b"href", b"link")
-                            | (b"src", b"audio")
-                            | (b"src", b"embed")
-                            | (b"src", b"iframe")
-                            | (b"src", b"img")
-                            | (b"src", b"input")
-                            | (b"src", b"script")
-                            | (b"src", b"source")
-                            | (b"src", b"track")
-                            | (b"src", b"video")
-                            | (b"srcset", b"img")
-                            | (b"srcset", b"source")
-                            | (b"cite", b"blockquote")
-                            | (b"cite", b"del")
-                            | (b"cite", b"ins")
-                            | (b"cite", b"q")
-                            | (b"data", b"object")
-                            | (b"onhashchange", b"body") => {
-                                urls.push(String::from_utf8_lossy(attr.value.as_ref()).to_string());
-                            }
-                            _ => {
-                                for link in extract_links_from_plaintext(
-                                    &String::from_utf8_lossy(attr.value.as_ref()).to_string(),
-                                ) {
-                                    urls.push(link);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            HTMLEvent::Text(txt) | HTMLEvent::Comment(txt) => {
-                for link in extract_links_from_plaintext(
-                    &String::from_utf8_lossy(txt.escaped()).to_string(),
-                ) {
-                    urls.push(link);
-                }
-            }
-            HTMLEvent::Eof => {
-                break;
-            }
-            _ => {}
-        }
-        buf.clear();
-    }
+    // we pass mutable urls reference to avoid extra allocations in each
+    // recursive descent
+    walk_html_links(&mut urls, &rc_dom.document);
+
     urls
 }
 
-// Extracting unparsed URL strings from a plaintext
+/// Recursively walk links in a HTML document, aggregating URL strings in `urls`.
+fn walk_html_links(mut urls: &mut Vec<String>, node: &Handle) {
+    match node.data {
+        NodeData::Text { ref contents } => {
+            // escape_default turns tab characters into "\t", newlines into "\n", etc.
+            let esc_contents = contents.borrow().escape_default().to_string();
+            for link in extract_links_from_plaintext(&esc_contents) {
+                urls.push(link);
+            }
+        }
+
+        NodeData::Comment { ref contents } => {
+            for link in extract_links_from_plaintext(&contents.escape_default().to_string()) {
+                urls.push(link);
+            }
+        }
+
+        NodeData::Element {
+            ref name,
+            ref attrs,
+            ..
+        } => {
+            for attr in attrs.borrow().iter() {
+                let attr_value = attr.value.escape_default().to_string();
+
+                if elem_attr_is_link(attr.name.local.as_ref(), name.local.as_ref()) {
+                    urls.push(attr_value);
+                } else {
+                    for link in extract_links_from_plaintext(&attr_value) {
+                        urls.push(link);
+                    }
+                }
+            }
+        }
+
+        _ => {}
+    }
+
+    // recursively traverse the document's nodes -- this doesn't need any extra
+    // exit conditions because the document is a tree
+    for child in node.children.borrow().iter() {
+        walk_html_links(&mut urls, child);
+    }
+}
+
+/// Determine if element's attribute contains a link / URL.
+fn elem_attr_is_link(attr_name: &str, elem_name: &str) -> bool {
+    matches!(
+        (attr_name, elem_name),
+        ("href", "a")
+            | ("href", "area")
+            | ("href", "base")
+            | ("href", "link")
+            | ("src", "audio")
+            | ("src", "embed")
+            | ("src", "iframe")
+            | ("src", "img")
+            | ("src", "input")
+            | ("src", "script")
+            | ("src", "source")
+            | ("src", "track")
+            | ("src", "video")
+            | ("srcset", "img")
+            | ("srcset", "source")
+            | ("cite", "blockquote")
+            | ("cite", "del")
+            | ("cite", "ins")
+            | ("cite", "q")
+            | ("data", "object")
+            | ("onhashchange", "body")
+    )
+}
+
+/// Extract unparsed URL strings from a plaintext.
 fn extract_links_from_plaintext(input: &str) -> Vec<String> {
     find_links(input)
         .iter()
@@ -311,10 +332,7 @@ mod test {
             Uri::Website(Url::parse("https://example.com/images/icon.png").unwrap()),
             Uri::Website(Url::parse("https://example.com/css/style_relative_url.css").unwrap()),
             Uri::Website(Url::parse("https://example.com/css/style_full_url.css").unwrap()),
-            // TODO BUG: the JS link is missing because the parser can't properly deal
-            //           with `<script defer src="..."></script>` (tags that have attributes with no value)
-            // Uri::Website(Url::parse("https://example.com/js/script.js").unwrap()),
-
+            Uri::Website(Url::parse("https://example.com/js/script.js").unwrap()),
             // the body links wouldn't be present if the file was parsed strictly as XML
             Uri::Website(Url::parse("https://example.com/body/a").unwrap()),
             Uri::Website(Url::parse("https://example.com/body/div_empty_a").unwrap()),
