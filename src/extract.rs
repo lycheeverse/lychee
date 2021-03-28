1fcb47611d157ebc2450f56ba162a8009285909b
Matthias Endler <matthias-endler@gmx.net>
2021-03-29 01:18:10 +0200

    Assume HTML in case there is no extension (e.g. for URLs)

--- src/extract.rs
+++ src/extract.rs
@@ -32,7 +32,7 @@ impl<P: AsRef<Path>> From<P> for FileType {
                 _ if (ext == "htm" || ext == "html") => FileType::Html,
                 _ => FileType::Plaintext,
             },
-            None => FileType::Plaintext,
+            None => FileType::Html,
         }
     }
 }
@@ -204,6 +204,9 @@ mod test {
 
     #[test]
     fn test_file_type() {
+        // Assume HTML in case there is no extension (that's including URLs
+        // without a path)
+        assert_eq!(FileType::from(Path::new("/")), FileType::Html);
         assert_eq!(FileType::from(Path::new("test.md")), FileType::Markdown);
         assert_eq!(
             FileType::from(Path::new("test.markdown")),
