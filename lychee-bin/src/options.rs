712bdfa8cbd55bdfa103e16ad3cf063e493af18a
Matthias <matthias-endler@gmx.net>
2021-09-16 16:40:38 +0200

    Make inputs required (show help if not provided) (#329)

--- lychee-bin/src/options.rs
+++ lychee-bin/src/options.rs
@@ -88,8 +88,8 @@ pub(crate) struct LycheeOptions {
     /// The inputs (where to get links to check from).
     /// These can be: files (e.g. `README.md`), file globs (e.g. `"~/git/*/README.md"`),
     /// remote URLs (e.g. `https://example.org/README.md`) or standard input (`-`).
-    /// Prefix with `--` to separate inputs from options that allow multiple arguments.
-    #[structopt(name = "inputs", default_value = "README.md")]
+    /// NOTE: Use `--` to separate inputs from options that allow multiple arguments.
+    #[structopt(name = "inputs", required = true)]
     raw_inputs: Vec<String>,
 
     /// Configuration file to use
