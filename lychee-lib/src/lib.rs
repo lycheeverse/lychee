56726f41fcdfeb1686f924ac56921a2db0fcd934
Matthias <matthias-endler@gmx.net>
2021-10-08 13:08:44 +0200

    Add back connection pool (#355)

--- lychee-lib/src/lib.rs
+++ lychee-lib/src/lib.rs
@@ -47,6 +47,7 @@
 doc_comment::doctest!("../../README.md");
 
 mod client;
+mod client_pool;
 /// A pool of clients, to handle concurrent checks
 pub mod collector;
 mod helpers;
@@ -72,7 +73,8 @@ use ring as _; // required for apple silicon
 
 #[doc(inline)]
 pub use crate::{
-    client::{check, Client, ClientBuilder},
+    client::{check, ClientBuilder},
+    client_pool::ClientPool,
     collector::Collector,
     filter::{Excludes, Filter, Includes},
     types::{Base, ErrorKind, Input, Request, Response, ResponseBody, Result, Status, Uri},
