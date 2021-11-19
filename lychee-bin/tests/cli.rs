375e6f41b753516854c19ed3629a2959690fc4c5
Matthias <matthias-endler@gmx.net>
2021-11-20 00:35:42 +0100

    Use thiserror for error handling
    This removes some boilerplate and is arguably better
    than handwriting the error handling code for
    maintainability and avoid inconsitent functionality
    for the error variants.
    thiserror is also the de-facto standard for library
    error types as of today.

--- lychee-bin/tests/cli.rs
+++ lychee-bin/tests/cli.rs
@@ -316,7 +316,7 @@ mod cli {
             .failure()
             .code(1)
             .stderr(contains(format!(
-                "Error: Failed to read file: `{}`, reason: No such file or directory (os error 2)",
+                "Error: Failed to read from path: `{}`, reason: No such file or directory (os error 2)",
                 filename
             )));
     }
