1926cd0a80a1cb9333c912965ce0284b288c5765
Matthias <matthias-endler@gmx.net>
2022-01-05 00:13:22 +0100

    Add missing test for local links in plaintext files

--- lychee-lib/src/extract/plaintext.rs
+++ lychee-lib/src/extract/plaintext.rs
@@ -12,6 +12,19 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_extract_local_links() {
+        let input = "http://127.0.0.1/ and http://127.0.0.1:8888/ are local links.";
+        let links: Vec<RawUri> = extract_plaintext(input);
+        assert_eq!(
+            links,
+            [
+                RawUri::from("http://127.0.0.1/"),
+                RawUri::from("http://127.0.0.1:8888/")
+            ]
+        );
+    }
+
+    #[test]
     fn test_extract_link_at_end_of_line() {
         let input = "https://www.apache.org/licenses/LICENSE-2.0\n";
         let uri = RawUri::from(input.trim_end());
