9f36e0125375ad5763ce546fe05e863fc811ca63
Matthias <matthias-endler@gmx.net>
2022-01-14 23:20:29 +0100

    update

--- examples/recursion/recursion.rs
+++ examples/recursion/recursion.rs
@@ -12,7 +12,7 @@ use tokio_stream::StreamExt;
 #[allow(clippy::trivial_regex)]
 async fn main() -> Result<()> {
     // Collect all links from the following inputs
-    let inputs = vec![
+    let inputs = [
         Input {
             source: InputSource::RemoteUrl(Box::new(
                 Url::parse("https://github.com/lycheeverse/lychee").unwrap(),
@@ -29,7 +29,7 @@ async fn main() -> Result<()> {
         None,  // base
         false, // don't skip missing inputs
     )
-    .from_vec(inputs)
+    .from_iter(inputs)
     .await
     .collect::<Result<Vec<_>>>()
     .await?;
