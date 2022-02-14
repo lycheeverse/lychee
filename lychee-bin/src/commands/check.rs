3d5eb4a5c391a0e5497df5063ca968c56c6565e4
Matthias <matthias-endler@gmx.net>
2022-02-14 13:05:30 +0100

    fmt

--- lychee-bin/src/commands/check.rs
+++ lychee-bin/src/commands/check.rs
@@ -94,9 +94,7 @@ where
     // the show_results_task to finish
     drop(send_req);
 
-    let (pb, stats) = show_results_task
-        .await
-        .map_err(ErrorKind::RuntimeJoin)?;
+    let (pb, stats) = show_results_task.await.map_err(ErrorKind::RuntimeJoin)?;
 
     // Note that print statements may interfere with the progress bar, so this
     // must go before printing the stats
