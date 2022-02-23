lycheeverse/lychee

commit ba276cd51ba66ecdbdb26243fb2efa59172fc478

Error cleanup (#510)
* Add more fine-grained error types; remove generic IO error
* Update error message for missing file
* Remove missing `Error` suffix
* Rename ErrorKind::Github to ErrorKind::GithubRequest for consistency with NetworkRequest

lychee-lib/src/types/cache.rs
@@ -22,10 +22,10 @@ pub enum CacheStatus {
 impl Display for CacheStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Success => write!(f, "Success"),
-            Self::Fail => write!(f, "Fail"),
-            Self::Excluded => write!(f, "Excluded"),
-            Self::Unsupported => write!(f, "Unsupported"),
+            Self::Success => write!(f, "Success [cached]"),
+            Self::Fail => write!(f, "Fail [cached]"),
+            Self::Excluded => write!(f, "Excluded [cached]"),
+            Self::Unsupported => write!(f, "Unsupported [cached]"),
         }
     }
 }

lychee-lib/src/types/error.rs
@@ -3,6 +3,7 @@ use std::any::Any;
 use std::hash::Hash;
 use std::{convert::Infallible, path::PathBuf};
 use thiserror::Error;
+use tokio::task::JoinError;
 
 use super::InputContent;
 use crate::Uri;
@@ -12,26 +13,36 @@ use crate::Uri;
 #[derive(Error, Debug)]
 #[non_exhaustive]
 pub enum ErrorKind {
-    // TODO: maybe needs to be split; currently first element is `Some` only for
-    // reading files
-    /// Any form of I/O error occurred while reading from a given path.
-    #[error("Failed to read from path: `{}`, reason: {1}", match .0 {
-        Some(p) => p.to_str().unwrap_or("<MALFORMED PATH>"),
-        None => "<MALFORMED PATH>",
-    })]
-    IoError(Option<PathBuf>, std::io::Error),
+    /// Error while executing a future on the Tokio runtime
+    #[error("Task failed to execute to completion")]
+    RuntimeJoin(#[from] JoinError),
+    /// Error while converting a file to an input
+    #[error("Cannot read input content from file `{1}`")]
+    ReadFileInput(#[source] std::io::Error, PathBuf),
+    /// Error while reading stdin as input
+    #[error("Cannot read input content from stdin")]
+    ReadStdinInput(#[from] std::io::Error),
     /// Errors which can occur when attempting to interpret a sequence of u8 as a string
     #[error("Attempted to interpret an invalid sequence of bytes as a string")]
-    Utf8Error(#[from] std::str::Utf8Error),
-    /// Reqwest network error
-    #[error("Network error (reqwest): {0}")]
-    ReqwestError(#[from] reqwest::Error),
+    Utf8(#[from] std::str::Utf8Error),
+    /// Network error while making request
+    #[error("Network error while handling request")]
+    NetworkRequest(#[source] reqwest::Error),
+    /// Cannot read the body of the received response
+    #[error("Error reading response body")]
+    ReadResponseBody(#[source] reqwest::Error),
+    /// The network client required for making requests cannot be created
+    #[error("Error creating request client")]
+    BuildRequestClient(#[source] reqwest::Error),
     /// Network error while using Github API
-    #[error("Network error (GitHub client) {}", .0.as_ref().map_or(String::new(), std::string::ToString::to_string))]
-    GithubError(#[from] Option<octocrab::Error>),
+    #[error("Network error (GitHub client)")]
+    GithubRequest(#[from] octocrab::Error),
+    /// Invalid Github URL
+    #[error("Github URL is invalid: {0}")]
+    InvalidGithubUrl(String),
     /// The given string can not be parsed into a valid URL, e-mail address, or file path
-    #[error("Cannot parse {0} as website url / file path or mail address: ({1:?})")]
-    UrlParseError(String, (url::ParseError, Option<fast_chemail::ParseError>)),
+    #[error("Cannot parse string `{1}` as website url")]
+    ParseUrl(#[source] url::ParseError, String),
     /// The given URI cannot be converted to a file path
     #[error("Cannot find file {0}")]
     InvalidFilePath(Uri),
@@ -66,7 +77,7 @@ pub enum ErrorKind {
     InsecureURL(Uri),
     /// Error while sending/receiving messages from MPSC channel
     #[error("Cannot send/receive message from channel")]
-    ChannelError(#[from] tokio::sync::mpsc::error::SendError<InputContent>),
+    Channel(#[from] tokio::sync::mpsc::error::SendError<InputContent>),
     /// An URL with an invalid host was found
     #[error("URL is missing a host")]
     InvalidUrlHost,
@@ -75,15 +86,31 @@ pub enum ErrorKind {
     InvalidURI(Uri),
 }
 
+#[allow(clippy::match_same_arms)]
 impl PartialEq for ErrorKind {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (Self::IoError(p1, e1), Self::IoError(p2, e2)) => p1 == p2 && e1.kind() == e2.kind(),
-            (Self::ReqwestError(e1), Self::ReqwestError(e2)) => e1.to_string() == e2.to_string(),
-            (Self::GithubError(_e1), Self::GithubError(_e2)) => false, // hubcaps::Error doesn't impl PartialEq
-            (Self::UrlParseError(s1, e1), Self::UrlParseError(s2, e2)) => s1 == s2 && e1 == e2,
-            (Self::UnreachableEmailAddress(u1, ..), Self::UnreachableEmailAddress(u2, ..))
-            | (Self::InsecureURL(u1), Self::InsecureURL(u2)) => u1 == u2,
+            (Self::NetworkRequest(e1), Self::NetworkRequest(e2)) => {
+                e1.to_string() == e2.to_string()
+            }
+            (Self::ReadResponseBody(e1), Self::ReadResponseBody(e2)) => {
+                e1.to_string() == e2.to_string()
+            }
+            (Self::BuildRequestClient(e1), Self::BuildRequestClient(e2)) => {
+                e1.to_string() == e2.to_string()
+            }
+            (Self::RuntimeJoin(e1), Self::RuntimeJoin(e2)) => e1.to_string() == e2.to_string(),
+            (Self::ReadFileInput(e1, s1), Self::ReadFileInput(e2, s2)) => {
+                e1.kind() == e2.kind() && s1 == s2
+            }
+            (Self::ReadStdinInput(e1), Self::ReadStdinInput(e2)) => e1.kind() == e2.kind(),
+            (Self::GithubRequest(e1), Self::GithubRequest(e2)) => e1.to_string() == e2.to_string(),
+            (Self::InvalidGithubUrl(s1), Self::InvalidGithubUrl(s2)) => s1 == s2,
+            (Self::ParseUrl(s1, e1), Self::ParseUrl(s2, e2)) => s1 == s2 && e1 == e2,
+            (Self::UnreachableEmailAddress(u1, ..), Self::UnreachableEmailAddress(u2, ..)) => {
+                u1 == u2
+            }
+            (Self::InsecureURL(u1), Self::InsecureURL(u2)) => u1 == u2,
             (Self::InvalidGlobPattern(e1), Self::InvalidGlobPattern(e2)) => {
                 e1.msg == e2.msg && e1.pos == e2.pos
             }
@@ -96,30 +123,34 @@ impl PartialEq for ErrorKind {
 
 impl Eq for ErrorKind {}
 
+#[allow(clippy::match_same_arms)]
 impl Hash for ErrorKind {
     fn hash<H>(&self, state: &mut H)
     where
         H: std::hash::Hasher,
     {
         match self {
-            Self::IoError(p, e) => (p, e.kind()).hash(state),
-            Self::ReqwestError(e) => e.to_string().hash(state),
-            Self::GithubError(e) => e.type_id().hash(state),
+            Self::RuntimeJoin(e) => e.to_string().hash(state),
+            Self::ReadFileInput(e, s) => (e.kind(), s).hash(state),
+            Self::ReadStdinInput(e) => e.kind().hash(state),
+            Self::NetworkRequest(e) => e.to_string().hash(state),
+            Self::ReadResponseBody(e) => e.to_string().hash(state),
+            Self::BuildRequestClient(e) => e.to_string().hash(state),
+            Self::GithubRequest(e) => e.type_id().hash(state),
+            Self::InvalidGithubUrl(s) => s.hash(state),
             Self::DirTraversal(e) => e.to_string().hash(state),
             Self::FileNotFound(e) => e.to_string_lossy().hash(state),
-            Self::UrlParseError(s, e) => (s, e.type_id()).hash(state),
+            Self::ParseUrl(e, s) => (e.type_id(), s).hash(state),
             Self::InvalidURI(u) => u.hash(state),
             Self::InvalidUrlFromPath(p) => p.hash(state),
-            Self::Utf8Error(e) => e.to_string().hash(state),
-            Self::InvalidFilePath(u)
-            | Self::UnreachableEmailAddress(u, ..)
-            | Self::InsecureURL(u) => {
-                u.hash(state);
-            }
+            Self::Utf8(e) => e.to_string().hash(state),
+            Self::InvalidFilePath(u) => u.hash(state),
+            Self::UnreachableEmailAddress(u, ..) => u.hash(state),
+            Self::InsecureURL(u, ..) => u.hash(state),
             Self::InvalidBase(base, e) => (base, e).hash(state),
             Self::InvalidHeader(e) => e.to_string().hash(state),
             Self::InvalidGlobPattern(e) => e.to_string().hash(state),
-            Self::ChannelError(e) => e.to_string().hash(state),
+            Self::Channel(e) => e.to_string().hash(state),
             Self::MissingGitHubToken | Self::InvalidUrlHost => {
                 std::mem::discriminant(self).hash(state);
             }
@@ -133,42 +164,6 @@ impl Serialize for ErrorKind {
         S: Serializer,
     {
         serializer.collect_str(self)
-    }
-}
-
-impl From<(PathBuf, std::io::Error)> for ErrorKind {
-    fn from(value: (PathBuf, std::io::Error)) -> Self {
-        Self::IoError(Some(value.0), value.1)
-    }
-}
-
-impl From<std::io::Error> for ErrorKind {
-    fn from(e: std::io::Error) -> Self {
-        Self::IoError(None, e)
-    }
-}
-
-impl From<tokio::task::JoinError> for ErrorKind {
-    fn from(e: tokio::task::JoinError) -> Self {
-        Self::IoError(None, e.into())
-    }
-}
-
-impl From<url::ParseError> for ErrorKind {
-    fn from(e: url::ParseError) -> Self {
-        Self::UrlParseError("Cannot parse URL".to_string(), (e, None))
-    }
-}
-
-impl From<(String, url::ParseError)> for ErrorKind {
-    fn from(value: (String, url::ParseError)) -> Self {
-        Self::UrlParseError(value.0, (value.1, None))
-    }
-}
-
-impl From<(String, url::ParseError, fast_chemail::ParseError)> for ErrorKind {
-    fn from(value: (String, url::ParseError, fast_chemail::ParseError)) -> Self {
-        Self::UrlParseError(value.0, (value.1, Some(value.2)))
     }
 }
 

lychee-lib/src/types/response.rs
@@ -1,8 +1,8 @@
-use std::fmt::Display;
+use std::{error::Error, fmt::Display};
 
 use serde::Serialize;
 
-use crate::{InputSource, Status, Uri};
+use crate::{ErrorKind, InputSource, Status, Uri};
 
 /// Response type returned by lychee after checking a URI
 #[derive(Debug)]
@@ -50,18 +50,51 @@ pub struct ResponseBody {
     pub status: Status,
 }
 
+// Extract as much information from the underlying error conditions as possible
+// without being too verbose. Some dependencies (rightfully) don't expose all
+// error fields to downstream crates, which is why we have to defer to pattern
+// matching in these cases.
 impl Display for ResponseBody {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} {}", self.status.icon(), self.uri)?;
 
-        // TODO: Other errors?
         match &self.status {
             Status::Ok(code) | Status::Redirected(code) => {
                 write!(f, " [{}]", code)
             }
-            Status::Timeout(Some(code)) => write!(f, " [{}]", code),
-            Status::Error(e) => write!(f, ": {}", e),
-            _ => Ok(()),
+            Status::Timeout(Some(code)) => write!(f, "Timeout [{code}]"),
+            Status::Timeout(None) => write!(f, "Timeout"),
+            Status::UnknownStatusCode(code) => write!(f, "Unknown status code [{code}]"),
+            Status::Excluded => write!(f, "Excluded"),
+            Status::Unsupported(e) => write!(f, "Unsupported {}", e),
+            Status::Cached(status) => write!(f, "{status}"),
+            Status::Error(e) => {
+                let details = match e {
+                    ErrorKind::NetworkRequest(e) => {
+                        if let Some(status) = e.status() {
+                            status.to_string()
+                        } else {
+                            "No status code".to_string()
+                        }
+                    }
+                    ErrorKind::GithubRequest(e) => match e {
+                        octocrab::Error::GitHub { source, .. } => source.message.to_string(),
+                        _ => "".to_string(),
+                    },
+                    _ => {
+                        if let Some(source) = e.source() {
+                            source.to_string()
+                        } else {
+                            "".to_string()
+                        }
+                    }
+                };
+                if details.is_empty() {
+                    write!(f, ": {e}")
+                } else {
+                    write!(f, ": {e}: {details}")
+                }
+            }
         }
     }
 }

lychee-lib/src/types/status.rs
@@ -24,7 +24,7 @@ pub enum Status {
     /// Request was successful
     Ok(StatusCode),
     /// Failed request
-    Error(Box<ErrorKind>),
+    Error(ErrorKind),
     /// Request timed out
     Timeout(Option<StatusCode>),
     /// Got redirected to different resource
@@ -36,7 +36,7 @@ pub enum Status {
     /// The request type is currently not supported,
     /// for example when the URL scheme is `slack://` or `file://`
     /// See https://github.com/lycheeverse/lychee/issues/199
-    Unsupported(Box<ErrorKind>),
+    Unsupported(ErrorKind),
     /// Cached request status from previous run
     Cached(CacheStatus),
 }
@@ -44,15 +44,15 @@ pub enum Status {
 impl Display for Status {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Status::Ok(c) => write!(f, "OK ({})", c),
-            Status::Redirected(c) => write!(f, "Redirect ({})", c),
-            Status::UnknownStatusCode(c) => write!(f, "Unknown status: {}", c),
+            Status::Ok(c) => write!(f, "OK ({c})"),
+            Status::Redirected(c) => write!(f, "Redirect ({c})"),
+            Status::UnknownStatusCode(c) => write!(f, "Unknown status: {c}"),
             Status::Excluded => f.write_str("Excluded"),
-            Status::Timeout(Some(c)) => write!(f, "Timeout ({})", c),
+            Status::Timeout(Some(c)) => write!(f, "Timeout ({c})"),
             Status::Timeout(None) => f.write_str("Timeout"),
-            Status::Unsupported(e) => write!(f, "Unsupported: {}", e),
-            Status::Error(e) => write!(f, "Failed: {}", e),
-            Status::Cached(s) => write!(f, "Cached: {}", s),
+            Status::Unsupported(e) => write!(f, "Unsupported: {e}"),
+            Status::Error(e) => write!(f, "Failed: {e}"),
+            Status::Cached(s) => write!(f, "Cached: {s}"),
         }
     }
 }
@@ -138,7 +138,7 @@ impl Status {
 
 impl From<ErrorKind> for Status {
     fn from(e: ErrorKind) -> Self {
-        Self::Error(Box::new(e))
+        Self::Error(e)
     }
 }
 
@@ -147,16 +147,18 @@ impl From<reqwest::Error> for Status {
         if e.is_timeout() {
             Self::Timeout(e.status())
         } else if e.is_builder() {
-            Self::Unsupported(Box::new(ErrorKind::ReqwestError(e)))
+            Self::Unsupported(ErrorKind::BuildRequestClient(e))
+        } else if e.is_body() || e.is_decode() {
+            Self::Unsupported(ErrorKind::ReadResponseBody(e))
         } else {
-            Self::Error(Box::new(ErrorKind::ReqwestError(e)))
+            Self::Error(ErrorKind::NetworkRequest(e))
         }
     }
 }
 
 impl From<octocrab::Error> for Status {
     fn from(e: octocrab::Error) -> Self {
-        Self::Error(Box::new(e.into()))
+        Self::Error(ErrorKind::GithubRequest(e))
     }
 }
 

lychee-bin/src/writer/detailed.rs
@@ -42,7 +42,7 @@ impl Display for DetailedResponseStats {
         for (source, responses) in &stats.fail_map {
             // Using leading newlines over trailing ones (e.g. `writeln!`)
             // lets us avoid extra newlines without any additional logic.
-            write!(f, "\n\nErrors in {}", source)?;
+            write!(f, "\n\nErrors in {source}")?;
             for response in responses {
                 write!(f, "\n{}", color_response(response))?;
             }

lychee-bin/tests/cli.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod cli {
     use std::{
+        error::Error,
         fs::{self, File},
         io::Write,
         path::{Path, PathBuf},
@@ -8,10 +9,11 @@ mod cli {
 
     use assert_cmd::Command;
     use http::StatusCode;
-    use lychee_lib::Result;
     use predicates::str::contains;
     use pretty_assertions::assert_eq;
     use uuid::Uuid;
+
+    type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
     macro_rules! mock_server {
         ($status:expr $(, $func:tt ($($arg:expr),*))*) => {{
@@ -327,8 +329,11 @@ mod cli {
             .failure()
             .code(1)
             .stderr(contains(format!(
-            "Error: Failed to read from path: `{filename}`, reason: No such file or directory (os error 2)"
-        )));
+                "Cannot read input content from file `{filename}`"
+            )))
+            .stderr(contains(
+                "No such file or directory (os error 2)".to_string(),
+            ));
     }
 
     #[test]

lychee-bin/tests/local_files.rs
@@ -1,10 +1,11 @@
 #[cfg(test)]
 mod cli {
-    use std::{fs::File, io::Write};
+    use std::{error::Error, fs::File, io::Write};
 
     use assert_cmd::Command;
-    use lychee_lib::Result;
     use predicates::str::contains;
+
+    type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
     fn main_command() -> Command {
         // this gets the "main" binary name (e.g. `lychee`)

commit 776cda9205a73149426c44da1ef1d75c58e53827

Fix response formatting
Before, the response and status were concatenated, e.g.
"https://github.com/marcelosdmFail [cached]."
This is a regression from #510.
Now there is a colon and a space in between the URL and the status and the status code is prepended
[200] https://github.com/marcelosdm: OK [cached]

lychee-bin/src/cache.rs
@@ -22,9 +22,10 @@ impl From<&Status> for CacheValue {
     }
 }
 
-/// The cache stores previous response codes
-/// for faster checking. At the moment it is backed by `DashMap`, but this is an
-/// implementation detail, which may change in the future.
+/// The cache stores previous response codes for faster checking.
+///
+/// At the moment it is backed by `DashMap`, but this is an
+/// implementation detail, which should not be relied upon.
 pub(crate) type Cache = DashMap<Uri, CacheValue>;
 
 pub(crate) trait StoreExt {

lychee-bin/src/stats.rs
@@ -7,7 +7,7 @@ use crate::color::{DIM, GREEN, NORMAL, PINK, YELLOW};
 
 pub(crate) fn color_response(response: &ResponseBody) -> String {
     let out = match response.status {
-        Status::Ok(_) | Status::Cached(CacheStatus::Success) => GREEN.apply_to(response),
+        Status::Ok(_) | Status::Cached(CacheStatus::Ok(_)) => GREEN.apply_to(response),
         Status::Excluded
         | Status::Unsupported(_)
         | Status::Cached(CacheStatus::Excluded | CacheStatus::Unsupported) => {
@@ -15,7 +15,7 @@ pub(crate) fn color_response(response: &ResponseBody) -> String {
         }
         Status::Redirected(_) => NORMAL.apply_to(response),
         Status::UnknownStatusCode(_) | Status::Timeout(_) => YELLOW.apply_to(response),
-        Status::Error(_) | Status::Cached(CacheStatus::Fail) => PINK.apply_to(response),
+        Status::Error(_) | Status::Cached(CacheStatus::Fail(_)) => PINK.apply_to(response),
     };
     out.to_string()
 }
@@ -63,8 +63,8 @@ impl ResponseStats {
 
         if let Status::Cached(cache_status) = status {
             match cache_status {
-                CacheStatus::Success => self.successful += 1,
-                CacheStatus::Fail => self.failures += 1,
+                CacheStatus::Ok(_) => self.successful += 1,
+                CacheStatus::Fail(_) => self.failures += 1,
                 CacheStatus::Excluded | CacheStatus::Unsupported => self.excludes += 1,
             }
         }
@@ -74,7 +74,7 @@ impl ResponseStats {
             Status::Error(_)
                 | Status::Timeout(_)
                 | Status::Redirected(_)
-                | Status::Cached(CacheStatus::Fail)
+                | Status::Cached(CacheStatus::Fail(_))
         ) {
             let fail = self.fail_map.entry(source).or_default();
             fail.insert(response.1);

lychee-lib/src/types/cache.rs
@@ -10,9 +10,9 @@ use crate::Status;
 #[derive(Debug, Serialize, Deserialize, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub enum CacheStatus {
     /// The cached request delivered a valid response
-    Success,
+    Ok(u16),
     /// The cached request failed before
-    Fail,
+    Fail(Option<u16>),
     /// The request was excluded (skipped)
     Excluded,
     /// The protocol is not yet supported
@@ -22,8 +22,8 @@ pub enum CacheStatus {
 impl Display for CacheStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Success => write!(f, "Success [cached]"),
-            Self::Fail => write!(f, "Fail [cached]"),
+            Self::Ok(_) => write!(f, "OK [cached]"),
+            Self::Fail(_) => write!(f, "Fail [cached]"),
             Self::Excluded => write!(f, "Excluded [cached]"),
             Self::Unsupported => write!(f, "Unsupported [cached]"),
         }
@@ -37,10 +37,12 @@ impl From<&Status> for CacheStatus {
             // Reqwest treats unknown status codes as Ok(StatusCode).
             // TODO: Use accepted status codes to decide whether this is a
             // success or failure
-            Status::Ok(_) | Status::UnknownStatusCode(_) => Self::Success,
+            Status::Ok(code) | Status::UnknownStatusCode(code) => Self::Ok(code.as_u16()),
             Status::Excluded => Self::Excluded,
             Status::Unsupported(_) => Self::Unsupported,
-            Status::Redirected(_) | Status::Error(_) | Status::Timeout(_) => Self::Fail,
+            Status::Redirected(code) => Self::Fail(Some(code.as_u16())),
+            Status::Timeout(code) => Self::Fail(code.map(|code| code.as_u16())),
+            Status::Error(_) => Self::Fail(None),
         }
     }
 }

lychee-lib/src/types/error.rs
@@ -25,8 +25,8 @@ pub enum ErrorKind {
     /// Errors which can occur when attempting to interpret a sequence of u8 as a string
     #[error("Attempted to interpret an invalid sequence of bytes as a string")]
     Utf8(#[from] std::str::Utf8Error),
-    /// Network error while making request
-    #[error("Network error while handling request")]
+    /// Network error while handling request
+    #[error("Network error")]
     NetworkRequest(#[source] reqwest::Error),
     /// Cannot read the body of the received response
     #[error("Error reading response body")]
@@ -44,7 +44,7 @@ pub enum ErrorKind {
     #[error("Cannot parse string `{1}` as website url")]
     ParseUrl(#[source] url::ParseError, String),
     /// The given URI cannot be converted to a file path
-    #[error("Cannot find file {0}")]
+    #[error("Cannot find file")]
     InvalidFilePath(Uri),
     /// The given path cannot be converted to a URI
     #[error("Invalid path to URL conversion: {0}")]

lychee-lib/src/types/response.rs
@@ -56,25 +56,39 @@ pub struct ResponseBody {
 // matching in these cases.
 impl Display for ResponseBody {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {}", self.status.icon(), self.uri)?;
+        write!(
+            f,
+            "{} [{}] {}",
+            self.status.icon(),
+            self.status.code(),
+            self.uri
+        )?;
 
         match &self.status {
-            Status::Ok(code) | Status::Redirected(code) => {
-                write!(f, " [{}]", code)
-            }
-            Status::Timeout(Some(code)) => write!(f, "Timeout [{code}]"),
-            Status::Timeout(None) => write!(f, "Timeout"),
-            Status::UnknownStatusCode(code) => write!(f, "Unknown status code [{code}]"),
-            Status::Excluded => write!(f, "Excluded"),
-            Status::Unsupported(e) => write!(f, "Unsupported {}", e),
-            Status::Cached(status) => write!(f, "{status}"),
+            Status::Ok(code) => match code.canonical_reason() {
+                Some(reason) => write!(f, ": {reason}"),
+                None => write!(f, ": OK"),
+            },
+            Status::Redirected(code) => match code.canonical_reason() {
+                Some(reason) => write!(f, ": {reason}"),
+                None => write!(f, ": Redirected"),
+            },
+            Status::Timeout(Some(code)) => write!(f, ": Timeout [{code}]"),
+            Status::Timeout(None) => write!(f, ": Timeout"),
+            Status::UnknownStatusCode(code) => write!(f, ": Unknown status code [{code}]"),
+            Status::Excluded => write!(f, ": Excluded"),
+            Status::Unsupported(e) => write!(f, ": Unsupported {}", e),
+            Status::Cached(status) => write!(f, ": {status}"),
             Status::Error(e) => {
                 let details = match e {
                     ErrorKind::NetworkRequest(e) => {
                         if let Some(status) = e.status() {
-                            status.to_string()
+                            status
+                                .canonical_reason()
+                                .unwrap_or("<unknown status code>")
+                                .to_string()
                         } else {
-                            "No status code".to_string()
+                            "<no status code>".to_string()
                         }
                     }
                     ErrorKind::GithubRequest(e) => match e {

lychee-lib/src/types/status.rs
@@ -134,6 +134,40 @@ impl Status {
             Status::Cached(_) => ICON_CACHED,
         }
     }
+
+    /// Return the HTTP status code (if any)
+    #[must_use]
+    pub fn code(&self) -> String {
+        match self {
+            Status::Ok(code) | Status::Redirected(code) | Status::UnknownStatusCode(code) => {
+                code.as_str().to_string()
+            }
+            Status::Excluded => "EXCLUDED".to_string(),
+            Status::Error(e) => match e {
+                ErrorKind::NetworkRequest(e)
+                | ErrorKind::ReadResponseBody(e)
+                | ErrorKind::BuildRequestClient(e) => match e.status() {
+                    Some(code) => code.as_str().to_string(),
+                    None => "ERROR".to_string(),
+                },
+                _ => "ERROR".to_string(),
+            },
+            Status::Timeout(code) => match code {
+                Some(code) => code.as_str().to_string(),
+                None => "TIMEOUT".to_string(),
+            },
+            Status::Unsupported(_) => "IGNORED".to_string(),
+            Status::Cached(cache_status) => match cache_status {
+                CacheStatus::Ok(code) => code.to_string(),
+                CacheStatus::Fail(code) => match code {
+                    Some(code) => code.to_string(),
+                    None => "ERROR".to_string(),
+                },
+                CacheStatus::Excluded => "EXCLUDED".to_string(),
+                CacheStatus::Unsupported => "IGNORED".to_string(),
+            },
+        }
+    }
 }
 
 impl From<ErrorKind> for Status {