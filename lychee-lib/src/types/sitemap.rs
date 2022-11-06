8ed2804ca5a52f62bec6c1dd4edd0514d07c0a8b
Matthias <matthias-endler@gmx.net>
2022-11-06 01:56:36 +0100

    wip

--- lychee-lib/src/types/sitemap.rs
+++ lychee-lib/src/types/sitemap.rs
@@ -1,44 +1,43 @@
 //! Sitemap handling
 
-use crate::{ErrorKind, Result};
+use html5ever::tendril::fmt::Slice;
 use reqwest::Url;
 use sitemap::reader::{SiteMapEntity, SiteMapReader};
-use std::{
-    fs::File,
-    io::{BufReader, Read},
-};
 
 pub struct Sitemap {}
 
 impl Sitemap {
-    /// Parse a sitemap from a URL
-    async fn fetch(url: &Url) -> Result<SiteMapReader<impl Read>> {
-        let response = reqwest::get(url.clone())
+    async fn fetch(url: &Url) -> Result<bytes::Bytes, reqwest::Error> {
+        Ok(reqwest::get(url.as_str())
             .await
-            .map_err(|e| ErrorKind::NetworkRequest(e))?;
-        let body = response.bytes().await.map_err(ErrorKind::NetworkRequest)?;
-        let reader = BufReader::new(body.to_owned());
-        Ok(SiteMapReader::new(reader))
+            .unwrap()
+            .bytes()
+            .await
+            .unwrap()
+            .to_owned())
     }
-
-    pub fn urls(sitemap: Url) -> Vec<Url> {
+    pub async fn urls(root_sitemap: Url) -> Vec<Url> {
         let mut urls = Vec::new();
-        let mut sitemaps = Vec::new();
-        let mut errors = Vec::new();
-        let file = File::open("sitemap.xml").expect("Unable to open file.");
-        let parser = SiteMapReader::new(file);
-        for entity in parser {
-            match entity {
-                SiteMapEntity::Url(url_entry) => {
-                    if let Some(url) = url_entry.loc.get_url() {
-                        urls.push(url);
+        let mut sitemaps: Vec<Url> = vec![root_sitemap];
+
+        if let Some(sitemap) = sitemaps.pop() {
+            let parser = SiteMapReader::new(content.as_bytes());
+            for entity in parser {
+                match entity {
+                    SiteMapEntity::Url(url_entry) => {
+                        if let Some(url) = url_entry.loc.get_url() {
+                            urls.push(url);
+                        }
                     }
-                }
-                SiteMapEntity::SiteMap(sitemap_entry) => {
-                    sitemaps.push(sitemap_entry);
-                }
-                SiteMapEntity::Err(error) => {
-                    errors.push(error);
+                    SiteMapEntity::SiteMap(sitemap_entry) => {
+                        if let Some(url) = sitemap_entry.loc.get_url() {
+                            sitemaps.push(url);
+                        }
+                    }
+                    SiteMapEntity::Err(error) => {
+                        // Silently ignore errors
+                        // errors.push(error);
+                    }
                 }
             }
         }
