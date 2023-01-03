5654b7c317e991c834b3854a9deba2d4c2e18218
Matthias Endler <matthias-endler@gmx.net>
2023-01-04 00:38:19 +0100

    Harden URL detection and extend verbatim elements (#899)
    Previously remote URLs were incorrectly detected because the
    string representation of a path is different than the path itself,
    causing the `http` prefix match to be insufficient.
    
    This resulted in unexpected side-effects, such as the
    incorrect detection of verbatim mode for remote URLs.
    
    The check now got improved and unit tests were added to avoid
    future breakage. On top of that, missing verbatim elements were added

--- lychee-lib/src/types/file.rs
+++ lychee-lib/src/types/file.rs
@@ -19,18 +19,22 @@ impl Default for FileType {
 
 impl<P: AsRef<Path>> From<P> for FileType {
     /// Detect if the given path points to a Markdown, HTML, or plaintext file.
+    //
+    // Assume HTML in case of no extension.
+    //
+    // This is only reasonable for URLs, not paths on disk. For example,
+    // a file named `README` without an extension is more likely to be a
+    // plaintext file.
+    //
+    // A better solution would be to also implement `From<Url> for
+    // FileType`. Unfortunately that's not possible without refactoring, as
+    // `AsRef<Path>` could be implemented for `Url` in the future, which is
+    // why `From<Url> for FileType` is not allowed (orphan rule).
+    //
+    // As a workaround, we check if the scheme is `http` or `https` and
+    // assume HTML in that case.
     fn from(p: P) -> FileType {
         let path = p.as_ref();
-        // Assume HTML in case of no extension.
-        // Note: this is only reasonable for URLs; not paths on disk.
-        // For example, `README` without an extension is more likely to be a plaintext file.
-        // A better solution would be to also implement `From<Url> for FileType`.
-        // Unfortunately that's not possible without refactoring, as
-        // `AsRef<Path>` could be implemented for `Url` in the future, which is why
-        // `From<Url> for FileType` is not allowed.
-        // As a workaround, we check if we got a known web-protocol
-        let is_url = path.starts_with("http");
-
         match path
             .extension()
             .and_then(std::ffi::OsStr::to_str)
@@ -42,10 +46,17 @@ impl<P: AsRef<Path>> From<P> for FileType {
                 FileType::Markdown
             }
             Some("htm" | "html") => FileType::Html,
-            None if is_url => FileType::Html,
-            _ => FileType::Plaintext,
+            None if is_url(path) => FileType::Html,
+            _ => FileType::default(),
         }
     }
+}
+
+/// Helper function to check if a path is likely a URL.
+fn is_url(path: &Path) -> bool {
+    path.to_str().map_or(false, |s| {
+        s.starts_with("http://") || s.starts_with("https://")
+    })
 }
 
 #[cfg(test)]
@@ -72,5 +83,14 @@ mod tests {
             FileType::from(Path::new("http://foo.com/index.html")),
             FileType::Html
         );
+    }
+
+    #[test]
+    fn test_is_url() {
+        assert!(is_url(Path::new("http://foo.com")));
+        assert!(is_url(Path::new("https://foo.com")));
+        assert!(!is_url(Path::new("foo.com")));
+        assert!(!is_url(Path::new("foo")));
+        assert!(!is_url(Path::new("foo/bar")));
     }
 }
