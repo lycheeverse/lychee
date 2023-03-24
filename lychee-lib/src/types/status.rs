96dd77b1d3b2e47b5f29681cdb4af7ca2770ac92
Benny <villiger@protonmail.com>
2023-03-24 12:19:03 +0100

    added tests for serialization and get status code

--- lychee-lib/src/types/status.rs
+++ lychee-lib/src/types/status.rs
@@ -281,3 +281,35 @@ impl From<reqwest::Error> for Status {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use http::StatusCode;
+    use crate::{CacheStatus, ErrorKind, Status};
+
+    #[test]
+    fn test_status_serialization() {
+        let status_ok = Status::Ok(StatusCode::from_u16(200).unwrap());
+        let serialized_with_code = serde_json::to_string(&status_ok).unwrap();
+        assert_eq!("{\"text\":\"OK (200 OK)\",\"code\":200}", serialized_with_code);
+
+        let status_timeout = Status::Timeout(None);
+        let serialized_without_code = serde_json::to_string(&status_timeout).unwrap();
+        assert_eq!("{\"text\":\"Timeout\"}", serialized_without_code)
+    }
+
+    #[test]
+    fn test_get_status_code() {
+        assert_eq!(Status::Ok(StatusCode::from_u16(200).unwrap()).code().unwrap(), 200);
+        assert_eq!(Status::Timeout(Some(StatusCode::from_u16(408).unwrap())).code().unwrap(), 408);
+        assert_eq!(Status::UnknownStatusCode(StatusCode::from_u16(999).unwrap()).code().unwrap(), 999);
+        assert_eq!(Status::Redirected(StatusCode::from_u16(300).unwrap()).code().unwrap(), 300);
+        assert_eq!(Status::Cached(CacheStatus::Ok(200)).code().unwrap(), 200);
+        assert_eq!(Status::Cached(CacheStatus::Error(Some(404))).code().unwrap(), 404);
+        assert_eq!(Status::Timeout(None).code(), None);
+        assert_eq!(Status::Cached(CacheStatus::Error(None)).code(), None);
+        assert_eq!(Status::Excluded.code(), None);
+        assert_eq!(Status::Unsupported(ErrorKind::InvalidStatusCode(999)).code(), None);
+    }
+}
+
