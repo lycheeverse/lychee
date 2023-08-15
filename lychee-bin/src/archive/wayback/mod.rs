06f798ef81d2337714b46200fccdd9ce76e2c48c
Matthias <matthias-endler@gmx.net>
2023-08-15 16:15:08 +0200

    Update dependencies; fix flaky tests

--- lychee-bin/src/archive/wayback/mod.rs
+++ lychee-bin/src/archive/wayback/mod.rs
@@ -56,30 +56,61 @@ where
 #[cfg(test)]
 mod tests {
     use crate::archive::wayback::get_wayback_link;
-    use reqwest::Error;
+    use reqwest::{Error, Url};
     use std::{error::Error as StdError, time::Duration};
     use tokio::time::sleep;
 
     #[tokio::test]
     async fn wayback_suggestion() -> Result<(), Box<dyn StdError>> {
-        let url = "https://example.com".parse()?;
+        let target_url = "https://example.com".parse::<Url>()?;
+
+        // Extract domain from target_url without the scheme and trailing slash
+        let expected_ending = (target_url.host_str().ok_or("Invalid target URL")?).to_string();
 
         // This test can be flaky, because the wayback machine does not always
         // return a suggestion. Retry a few times if needed.
         for _ in 0..3 {
-            if let Some(suggestion) = get_wayback_link(&url).await? {
-                assert_eq!(
-                    suggestion
+            match get_wayback_link(&target_url).await {
+                Ok(Some(suggested_url)) => {
+                    // Ensure the host is correct
+                    let host = suggested_url
                         .host_str()
-                        .expect("Suggestion doesn't have a host"),
-                    "web.archive.org"
-                );
-                assert!(suggestion.path().ends_with(url.as_str()));
-                return Ok(());
+                        .ok_or("Suggestion doesn't have a host")?;
+                    assert_eq!(host, "web.archive.org");
+
+                    // Extract the actual archived URL from the Wayback URL
+                    let archived_url = suggested_url
+                        .path()
+                        .trim_start_matches("/web/")
+                        .split_once('/')
+                        .map(|x| x.1)
+                        .ok_or("Failed to extract archived URL from Wayback suggestion")?;
+
+                    // Check the ending of the suggested URL without considering trailing slash
+                    if !archived_url
+                        .trim_end_matches('/')
+                        .ends_with(&expected_ending)
+                    {
+                        return Err(format!(
+                            "Expected suggestion '{archived_url}' to end with '{expected_ending}'"
+                        )
+                        .into());
+                    }
+
+                    return Ok(());
+                }
+                Ok(None) => {
+                    // No suggestion was returned, wait and retry
+                    sleep(Duration::from_secs(1)).await;
+                }
+                Err(e) => {
+                    // Propagate other errors
+                    return Err(format!("Error retrieving Wayback link: {e}").into());
+                }
             }
-            sleep(Duration::from_secs(1)).await; // add delay between retries
         }
-        Err("Did not get a valid Wayback Machine suggestion.".into())
+
+        Err("Did not get a valid Wayback Machine suggestion after multiple attempts.".into())
     }
 
     #[tokio::test]
