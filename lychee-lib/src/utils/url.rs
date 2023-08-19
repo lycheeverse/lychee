9da9731f921f8fdb929be639adda79858a91ffc1
Viktor Szépe <viktor@szepe.net>
2023-08-19 12:41:53 +0000

    Fix typos

--- lychee-lib/src/utils/url.rs
+++ lychee-lib/src/utils/url.rs
@@ -4,9 +4,9 @@ use once_cell::sync::Lazy;
 
 static LINK_FINDER: Lazy<LinkFinder> = Lazy::new(LinkFinder::new);
 
-/// Remove all GET parameters from a URL and seperates out the fragment.
+/// Remove all GET parameters from a URL and separates out the fragment.
 /// The link is not a URL but a String as it may not have a base domain.
-pub(crate) fn remove_get_params_and_seperate_fragment(url: &str) -> (&str, Option<&str>) {
+pub(crate) fn remove_get_params_and_separate_fragment(url: &str) -> (&str, Option<&str>) {
     let (path, frag) = match url.split_once('#') {
         Some((path, fragment)) => (path, Some(fragment)),
         None => (url, None),
@@ -29,48 +29,48 @@ mod test_fs_tree {
 
     #[test]
     fn test_remove_get_params_and_fragment() {
-        assert_eq!(remove_get_params_and_seperate_fragment("/"), ("/", None));
+        assert_eq!(remove_get_params_and_separate_fragment("/"), ("/", None));
         assert_eq!(
-            remove_get_params_and_seperate_fragment("index.html?foo=bar"),
+            remove_get_params_and_separate_fragment("index.html?foo=bar"),
             ("index.html", None)
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment("/index.html?foo=bar"),
+            remove_get_params_and_separate_fragment("/index.html?foo=bar"),
             ("/index.html", None)
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment("/index.html?foo=bar&baz=zorx?bla=blub"),
+            remove_get_params_and_separate_fragment("/index.html?foo=bar&baz=zorx?bla=blub"),
             ("/index.html", None)
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment("https://example.com/index.html?foo=bar"),
+            remove_get_params_and_separate_fragment("https://example.com/index.html?foo=bar"),
             ("https://example.com/index.html", None)
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment("test.png?foo=bar"),
+            remove_get_params_and_separate_fragment("test.png?foo=bar"),
             ("test.png", None)
         );
 
         assert_eq!(
-            remove_get_params_and_seperate_fragment("https://example.com/index.html#anchor"),
+            remove_get_params_and_separate_fragment("https://example.com/index.html#anchor"),
             ("https://example.com/index.html", Some("anchor"))
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment(
+            remove_get_params_and_separate_fragment(
                 "https://example.com/index.html?foo=bar#anchor"
             ),
             ("https://example.com/index.html", Some("anchor"))
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment("test.png?foo=bar#anchor"),
+            remove_get_params_and_separate_fragment("test.png?foo=bar#anchor"),
             ("test.png", Some("anchor"))
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment("test.png#anchor?anchor!?"),
+            remove_get_params_and_separate_fragment("test.png#anchor?anchor!?"),
             ("test.png", Some("anchor?anchor!?"))
         );
         assert_eq!(
-            remove_get_params_and_seperate_fragment("test.png?foo=bar#anchor?anchor!"),
+            remove_get_params_and_separate_fragment("test.png?foo=bar#anchor?anchor!"),
             ("test.png", Some("anchor?anchor!"))
         );
     }
