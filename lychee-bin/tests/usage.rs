df687c8e34d92369288179d3947c2c93559a7dc3
Matthias <matthias@endler.dev>
2024-02-05 16:14:49 +0100

    Fix usage test by filtering out lychee version number

--- lychee-bin/tests/usage.rs
+++ lychee-bin/tests/usage.rs
@@ -31,14 +31,34 @@ mod readme {
         let mut cmd = main_command();
 
         let help_cmd = cmd.env_clear().arg("--help").assert().success();
-        let help_output = std::str::from_utf8(&help_cmd.get_output().stdout).unwrap();
-        let usage_in_help_start = help_output.find(USAGE_STRING).unwrap();
+        let help_output = std::str::from_utf8(&help_cmd.get_output().stdout)?;
+        let usage_in_help_start = help_output
+            .find(USAGE_STRING)
+            .ok_or("Usage not found in help")?;
         let usage_in_help = &help_output[usage_in_help_start..];
 
+        // Remove line `[default: lychee/0.1.0]` from the help output
+        let usage_in_help = usage_in_help
+            .lines()
+            .filter(|line| !line.contains("[default: lychee/"))
+            .collect::<Vec<_>>()
+            .join("\n");
+
         let readme = load_readme_text();
-        let usage_start = readme.find(USAGE_STRING).unwrap();
-        let usage_end = readme[usage_start..].find("\n```").unwrap();
+        let usage_start = readme
+            .find(USAGE_STRING)
+            .ok_or("Usage not found in README")?;
+        let usage_end = readme[usage_start..]
+            .find("\n```")
+            .ok_or("End of usage not found in README")?;
         let usage_in_readme = &readme[usage_start..usage_start + usage_end];
+
+        // Remove line `[default: lychee/0.1.0]` from the README
+        let usage_in_readme = usage_in_readme
+            .lines()
+            .filter(|line| !line.contains("[default: lychee/"))
+            .collect::<Vec<_>>()
+            .join("\n");
 
         assert_eq!(usage_in_readme, usage_in_help);
         Ok(())
