70fc0186f4ef16de3f02f7b660e5d05b0aeb0ec2
Matthias <matthias@endler.dev>
2024-02-17 15:22:56 +0100

    it works

--- lychee-bin/src/formatters/response.rs
+++ lychee-bin/src/formatters/response.rs
@@ -1,13 +1,17 @@
-use lychee_lib::{CacheStatus, Response, ResponseBody, Result, Status};
+use std::io::Write;
+
+use futures::io::BufWriter;
+use http::StatusCode;
+use lychee_lib::{CacheStatus, ResponseBody, Status};
 
 use crate::color::{DIM, GREEN, NORMAL, PINK, YELLOW};
 
 /// A `ResponseFormatter` knows how to format a response for different output
 /// preferences based on user settings or the environment
-pub(crate) trait ResponseFormatter: Send + Sync {
-    /// Format a single link check response and write it to stdout
-    fn write_response(&self, response: &Response) -> Result<String>;
-}
+// pub(crate) trait ResponseFormatter: Send + Sync {
+//     /// Format a single link check response and write it to stdout
+//     fn write_response(&self, response: &Response) -> Result<String>;
+// }
 
 /// A trait for formatting a response body
 ///
@@ -27,7 +31,7 @@ pub(crate) struct BasicFormatter;
 
 impl ResponseBodyFormatter for BasicFormatter {
     fn format_response(&self, body: &ResponseBody) -> String {
-        body.to_string()
+        format!("{}", body)
     }
 }
 
@@ -39,17 +43,45 @@ pub(crate) struct ColorFormatter;
 
 impl ResponseBodyFormatter for ColorFormatter {
     fn format_response(&self, body: &ResponseBody) -> String {
-        let out = match body.status {
-            Status::Ok(_) | Status::Cached(CacheStatus::Ok(_)) => GREEN.apply_to(body),
+        match body.status {
+            Status::Ok(_) | Status::Cached(CacheStatus::Ok(_)) => {
+                // GREEN.apply_to(body),
+
+                // Format the status code in green
+                // and the response body in normal text
+                // format!("{}{}", GREEN.apply_to(&body.status), NORMAL.apply_to(body))
+                let status = format!("{} [{}]", body.status.icon(), body.status.code_as_string(),);
+
+                let mut output = format!("{} {}", GREEN.apply_to(status), body.uri);
+
+                if let Status::Ok(StatusCode::OK) = body.status {
+                    // Don't print anything else if the status code is 200.
+                    // The output gets too verbose then.
+                    return output;
+                }
+
+                // Add a separator between the URI and the additional details below.
+                // Note: To make the links clickable in some terminals,
+                // we add a space before the separator.
+                // write!(f, " | {}", self.status)?;
+                output.push_str(&format!(" | {}", body.status));
+
+                if let Some(details) = body.status.details() {
+                    // write!(f, ": {details}")
+                    output.push_str(&format!(": {}", details))
+                }
+                output
+            }
             Status::Excluded
             | Status::Unsupported(_)
             | Status::Cached(CacheStatus::Excluded | CacheStatus::Unsupported) => {
-                DIM.apply_to(body)
+                DIM.apply_to(body).to_string()
             }
-            Status::Redirected(_) => NORMAL.apply_to(body),
-            Status::UnknownStatusCode(_) | Status::Timeout(_) => YELLOW.apply_to(body),
-            Status::Error(_) | Status::Cached(CacheStatus::Error(_)) => PINK.apply_to(body),
-        };
-        out.to_string()
+            Status::Redirected(_) => NORMAL.apply_to(body).to_string(),
+            Status::UnknownStatusCode(_) | Status::Timeout(_) => YELLOW.apply_to(body).to_string(),
+            Status::Error(_) | Status::Cached(CacheStatus::Error(_)) => {
+                PINK.apply_to(body).to_string()
+            }
+        }
     }
 }
