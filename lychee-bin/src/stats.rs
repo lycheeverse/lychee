730f5310b1c1add05e3783a640abfe404e5b72df
Thomas Zahner <thomas.zahner@protonmail.ch>
2024-04-24 16:38:06 +0200

    Disable lint with false positive

--- lychee-bin/src/stats.rs
+++ lychee-bin/src/stats.rs
@@ -1,3 +1,6 @@
+// Disable lint, clippy thinks that InputSource has inner mutability, but this seems like a flase positive
+#![allow(clippy::mutable_key_type)]
+
 use std::collections::{HashMap, HashSet};
 
 use crate::archive::Suggestion;
