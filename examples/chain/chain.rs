e7af909a1a64f42f567d51ec882b97234e0fef67
Thomas Zahner <thomas.zahner@protonmail.ch>
2024-04-25 15:09:18 +0200

    Add chain usage example

---
+++ examples/chain/chain.rs
@@ -0,0 +1,34 @@
+use async_trait::async_trait;
+use lychee_lib::{chain::RequestChain, ChainResult, ClientBuilder, Handler, Result, Status};
+use reqwest::{Method, Request};
+
+#[derive(Debug)]
+struct MyHandler {}
+
+#[async_trait]
+impl Handler<Request, Status> for MyHandler {
+    async fn handle(&mut self, mut request: Request) -> ChainResult<Request, Status> {
+        // Handle special case of some website (fictional example)
+        if request.url().domain() == Some("wikipedia.org") && request.url().path() == "/home" {
+            request.url_mut().set_path("/foo-bar");
+            *request.method_mut() = Method::PUT;
+        }
+
+        ChainResult::Next(request)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let chain = RequestChain::new(vec![Box::new(MyHandler {})]);
+
+    let client = ClientBuilder::builder()
+        .plugin_request_chain(chain)
+        .build()
+        .client()?;
+
+    let result = client.check("https://wikipedia.org/home").await;
+    println!("{:?}", result);
+
+    Ok(())
+}
