81f4ce65478d7cd6a8f527519edf4a3e39742e86
Thomas Zahner <thomas.zahner@protonmail.ch>
2024-11-24 12:22:34 +0100

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -1,66 +1,47 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    inputs@{ self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-
-      perSystem =
-        {
-          config,
-          self',
-          inputs',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          _module.args.pkgs = import self.inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              (import inputs.rust-overlay)
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
+    in
+    {
+      devShells.${system}.default =
+        let
+          rustVersion = "latest"; # using a specific version: "1.62.0"
+          rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+            extensions = [
+              "rust-src" # for rust-analyzer
+              "rust-analyzer" # usable by IDEs like zed-editor
+              "clippy"
             ];
           };
-          devShells.default =
-            let
-              rustVersion = "latest"; # using a specific version: "1.62.0"
-              rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-                extensions = [
-                  "rust-src" # for rust-analyzer
-                  "rust-analyzer" # usable by IDEs like zed-editor
-                  "clippy"
-                ];
-              };
-              libPath =
-                with pkgs;
-                lib.makeLibraryPath [
-                  pkg-config
-                  openssl
-                ];
-
-            in
-            pkgs.mkShell {
-              inputsFrom = builtins.attrValues self'.packages;
-              packages = [
-                pkgs.pkg-config
-                pkgs.openssl
-                rust
-              ];
-              LD_LIBRARY_PATH = libPath;
-              RUST_BACKTRACE = 1;
-              LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            };
+          libPath = pkgs.lib.makeLibraryPath [
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+        in
+        pkgs.mkShell {
+          packages = [
+            pkgs.pkg-config
+            pkgs.openssl
+            rust
+          ];
+          LD_LIBRARY_PATH = libPath;
+          RUST_BACKTRACE = 1;
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
         };
     };
 }
