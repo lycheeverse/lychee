745fd9071efa339c6567f49a6607bf4cc6308b2a
Thomas Zahner <thomas.zahner@protonmail.ch>
2024-12-19 18:29:12 +0100

    More consistent style

--- flake.nix
+++ flake.nix
@@ -21,29 +21,31 @@
       rustVersion = "latest"; # using a specific version: "1.62.0"
     in
     {
-      devShells = forAllSystems (system: {
-        default =
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ (import rust-overlay) ];
-            };
-            rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-              extensions = [
-                "rust-src" # for rust-analyzer
-                "rust-analyzer" # usable by IDEs like zed-editor
-                "clippy"
-              ];
-            };
-          in
-          pkgs.mkShell {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+          rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+            extensions = [
+              "rust-src" # for rust-analyzer
+              "rust-analyzer" # usable by IDEs like zed-editor
+              "clippy"
+            ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
             packages = [
               pkgs.pkg-config
               pkgs.openssl
               rust
             ];
           };
-      });
+        }
+      );
 
       packages = forAllSystems (
         system:
