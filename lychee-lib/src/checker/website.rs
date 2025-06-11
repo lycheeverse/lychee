ad493dcdfd660c73a31a5f61c339bb437d696188
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-06-11 10:49:18 +0200

    Expand Status::Redirected

--- lychee-lib/src/checker/website.rs
+++ lychee-lib/src/checker/website.rs
@@ -116,8 +116,15 @@ impl WebsiteChecker {
                 if self.include_fragments && status.is_success() && method == Method::GET {
                     return self.check_html_fragment(status, response).await;
                 }
-                if let Some(redirection) = self.redirect_map.lock().unwrap().get(&url) {
-                    todo!("Redirected from {} to {}", url, redirection)
+
+                if let Some(code) = status.code() {
+                    if let Some(resolved) = self.redirect_map.lock().unwrap().get(&url).cloned() {
+                        return Status::Redirected {
+                            original: url,
+                            resolved,
+                            code,
+                        };
+                    }
                 }
 
                 status
