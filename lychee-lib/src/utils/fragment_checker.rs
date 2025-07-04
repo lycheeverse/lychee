3e13a8818a9db3c2d5022439502251a914b0d535
Keming <kemingy94@gmail.com>
2025-07-05 00:50:27 +0800

    extract fragment builder struct
    Signed-off-by: Keming <kemingy94@gmail.com>

--- lychee-lib/src/utils/fragment_checker.rs
+++ lychee-lib/src/utils/fragment_checker.rs
@@ -30,6 +30,61 @@ impl FragmentInput {
     }
 }
 
+/// A fragment builder that expands the given fragments into a list of candidates.
+struct FragmentBuilder {
+    variants: Vec<String>,
+    decoded: Vec<String>,
+}
+
+impl FragmentBuilder {
+    fn new(fragment: &str, url: &Url, file_type: FileType) -> Result<Self> {
+        let mut variants = vec![fragment.into()];
+        // For GitHub links, add "user-content-" prefix to the fragments.
+        // The following cases cannot be handled unless we simulate with a headless browser:
+        // - markdown files from any specific path (includes "blob/master/README.md")
+        // - "issuecomment" fragments from the GitHub issue pages
+        if url
+            .host_str()
+            .is_some_and(|host| host.ends_with("github.com"))
+        {
+            variants.push(format!("user-content-{fragment}"));
+        }
+
+        // Only store the percent-decoded variants if it's different from the original
+        // fragment. This avoids storing and comparing the same fragment twice.
+        let mut decoded = Vec::new();
+        for frag in &variants {
+            let mut require_alloc = false;
+            let mut fragment_decoded: Cow<'_, str> = match percent_decode_str(frag).decode_utf8()? {
+                Cow::Borrowed(s) => s.into(),
+                Cow::Owned(s) => {
+                    require_alloc = true;
+                    s.into()
+                }
+            };
+            if file_type == FileType::Markdown {
+                let lowercase = fragment_decoded.to_lowercase();
+                if lowercase != fragment_decoded {
+                    fragment_decoded = lowercase.into();
+                    require_alloc = true;
+                }
+            }
+            if require_alloc {
+                decoded.push(fragment_decoded.into());
+            }
+        }
+
+        Ok(Self { variants, decoded })
+    }
+
+    fn any_matches(&self, fragments: &HashSet<String>) -> bool {
+        self.variants
+            .iter()
+            .chain(self.decoded.iter())
+            .any(|frag| fragments.contains(frag))
+    }
+}
+
 /// Holds a cache of fragments for a given URL.
 ///
 /// Fragments, also known as anchors, are used to link to a specific
@@ -68,17 +123,7 @@ impl FragmentChecker {
         if fragment.is_empty() || fragment.eq_ignore_ascii_case("top") {
             return Ok(true);
         }
-        let mut fragment_candidates = vec![Cow::Borrowed(fragment)];
-        // For GitHub links, add "user-content-" prefix to the fragments.
-        // The following cases cannot be handled unless we simulate with a headless browser:
-        // - markdown files from any specific path (includes "blob/master/README.md")
-        // - "issuecomment" fragments from the GitHub issue pages
-        if url
-            .host_str()
-            .is_some_and(|host| host.ends_with("github.com"))
-        {
-            fragment_candidates.push(Cow::Owned(format!("user-content-{fragment}")));
-        }
+
         let url_without_frag = Self::remove_fragment(url.clone());
 
         let FragmentInput { content, file_type } = input;
@@ -88,30 +133,17 @@ impl FragmentChecker {
             FileType::Plaintext => return Ok(true),
         };
 
-        let mut all_fragments = Vec::with_capacity(2 * fragment_candidates.len());
-        for fragment in &fragment_candidates {
-            let mut fragment_decoded = percent_decode_str(fragment).decode_utf8()?;
-            if file_type == FileType::Markdown {
-                fragment_decoded = fragment_decoded.to_lowercase().into();
-            }
-            all_fragments.push(fragment_decoded);
-        }
-        all_fragments.extend(fragment_candidates.iter().cloned());
-
+        let fragment_candidates = FragmentBuilder::new(fragment, url, file_type)?;
         match self.cache.lock().await.entry(url_without_frag) {
             Entry::Vacant(entry) => {
                 let file_frags = extractor(&content);
-                let contains_fragment = all_fragments
-                    .iter()
-                    .any(|frag| file_frags.contains(frag.as_ref()));
+                let contains_fragment = fragment_candidates.any_matches(&file_frags);
                 entry.insert(file_frags);
                 Ok(contains_fragment)
             }
             Entry::Occupied(entry) => {
                 let file_frags = entry.get();
-                Ok(all_fragments
-                    .iter()
-                    .any(|frag| file_frags.contains(frag.as_ref())))
+                Ok(fragment_candidates.any_matches(file_frags))
             }
         }
     }
