ea415c8db4597c383f88b5fc9894fd86a7245494
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-07-18 18:50:51 +0200

    Fix test

--- lychee-bin/tests/cli.rs
+++ lychee-bin/tests/cli.rs
@@ -1928,9 +1928,9 @@ mod cli {
                 "https://raw.githubusercontent.com/lycheeverse/lychee/master/fixtures/fragments/zero.bin#fragment",
             ))
             .stdout(contains("42 Total"))
-            .stdout(contains("30 OK"))
+            .stdout(contains("31 OK"))
             // Failures because of missing fragments or failed binary body scan
-            .stdout(contains("12 Errors"));
+            .stdout(contains("11 Errors"));
     }
 
     #[test]
