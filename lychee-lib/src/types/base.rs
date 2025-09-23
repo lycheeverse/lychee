66e1670b7b7c2222d0afa7caba3105e9e1e589ba
rina <k@rina.fyi>
2025-09-23 21:30:17 +1000

    feat(base-url)!: disallow relative directory paths in `--base-url`
    the previous behaviour accepted relative directory paths as bases
    but this led to later InvalidBaseJoin errors, because relative paths
    cannot be used to join relative URLs. this means that relative local
    bases were *only* useful for resolving root-relative links, and were
    confusingly problematic with ordinary relative links.
    
    see https://www.github.com/lycheeverse/lychee/issues/1574 which talks about
    errors when passing `--base ../network-documentation/` or, in a later
    comment, `--base build`.
    
    also, the previous behaviour would parse something like
    `--base-url google.com` to a local base pointing to `./google.com`.
    this would also lead to downstream errors and it's better to guard
    against this.
    
    it is my opinion that it is better to fail earlier in these cases, so
    the user is not hit with mysterious InvalidBaseJoin.
    
    after this pr, there will be a command-line argument parsing error:
    ```
    error: invalid value 'build' for '--base-url <BASE_URL>': Error with
    base dir `build` : Base must either be a URL (with scheme) or an
    absolute path. Alternatively, if you want to resolve root-relative links
    in local files, see `--root-dir`.
    ```
    
    in a slightly opinionated touch, i've mentioned --root-dir in the error
    message, since i think --root-dir is more suitable for most use cases
    where people try to use relative local bases. this agrees with later
    comments in https://www.github.com/lycheeverse/lychee/issues/1574.
    
    however, this does make the error message quite long. so i'm happy to
    take on feedback and changes about this.
    
    this pr implements (1) in my outline to reduce InvalidBaseJoin
    confusion, as described in
    https://www.github.com/lycheeverse/lychee/pull/1624#issuecomment-3274485963

--- lychee-lib/src/types/base.rs
+++ lychee-lib/src/types/base.rs
@@ -61,7 +61,24 @@ impl TryFrom<&str> for Base {
             }
             return Ok(Self::Remote(url));
         }
-        Ok(Self::Local(PathBuf::from(value)))
+
+        // require absolute paths in `Base::Local`. a local non-relative base is
+        // basically useless because it cannot be used to join URLs and will
+        // cause InvalidBaseJoin.
+        let path = PathBuf::from(value);
+        if path.is_absolute() {
+            Ok(Self::Local(path))
+        } else {
+            Err(ErrorKind::InvalidBase(
+                value.to_string(),
+                vec![
+                    "Base must either be a URL (with scheme) or an absolute path.",
+                    "Alternatively, if you want to resolve root-relative links in",
+                    "local files, see `--root-dir`.",
+                ]
+                .join(" "),
+            ))
+        }
     }
 }
 
@@ -96,10 +113,20 @@ mod test_base {
 
     #[test]
     fn test_valid_local_path_string_as_base() -> Result<()> {
-        let cases = vec!["/tmp/lychee", "/tmp/lychee/", "tmp/lychee/"];
+        let cases = vec!["/tmp/lychee", "/tmp/lychee/"];
 
         for case in cases {
             assert_eq!(Base::try_from(case)?, Base::Local(PathBuf::from(case)));
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_invalid_local_path_string_as_base() -> Result<()> {
+        let cases = vec!["a", "tmp/lychee/", "example.com", "../nonlocal"];
+
+        for case in cases {
+            assert!(Base::try_from(case).is_err())
         }
         Ok(())
     }
