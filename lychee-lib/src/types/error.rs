d272a2b00b285bddbaeeff2e5761a36e2ba4f9ae
rina <k@rina.fyi>
2025-10-04 18:48:52 +1000

    feat: add CreateRequestItem error kind

--- lychee-lib/src/types/error.rs
+++ lychee-lib/src/types/error.rs
@@ -7,7 +7,9 @@ use thiserror::Error;
 use tokio::task::JoinError;
 
 use super::InputContent;
+use crate::ResolvedInputSource;
 use crate::types::StatusCodeSelectorError;
+use crate::types::uri::raw::RawUri;
 use crate::{Uri, basic_auth::BasicAuthExtractorError, utils};
 
 /// Kinds of status errors
@@ -25,6 +27,10 @@ pub enum ErrorKind {
     /// The network client required for making requests cannot be created
     #[error("Error creating request client: {0}")]
     BuildRequestClient(#[source] reqwest::Error),
+
+    /// TODO: a
+    #[error("Error creating request: {0}")]
+    CreateRequestItem(RawUri, ResolvedInputSource, #[source] Box<ErrorKind>),
 
     /// Network error while using GitHub API
     #[error("Network error (GitHub client)")]
@@ -249,6 +255,9 @@ impl ErrorKind {
             ErrorKind::BuildRequestClient(error) => Some(format!(
                 "Failed to create HTTP client: {error}. Check system configuration",
             )),
+            ErrorKind::CreateRequestItem(_, _, _) => Some(format!(
+                "TODO: details creat request utem"
+            )),
             ErrorKind::RuntimeJoin(join_error) => Some(format!(
                 "Task execution failed: {join_error}. Internal processing error"
             )),
@@ -374,6 +383,9 @@ impl PartialEq for ErrorKind {
             (Self::BuildRequestClient(e1), Self::BuildRequestClient(e2)) => {
                 e1.to_string() == e2.to_string()
             }
+            (Self::CreateRequestItem(uri1, s1, e1), Self::CreateRequestItem(uri2, s2, e2)) => {
+                uri1 == uri2 && s1 == s2 && e1.to_string() == e2.to_string()
+            }
             (Self::RuntimeJoin(e1), Self::RuntimeJoin(e2)) => e1.to_string() == e2.to_string(),
             (Self::ReadFileInput(e1, s1), Self::ReadFileInput(e2, s2)) => {
                 e1.kind() == e2.kind() && s1 == s2
@@ -433,6 +445,7 @@ impl Hash for ErrorKind {
             Self::NetworkRequest(e) => e.to_string().hash(state),
             Self::ReadResponseBody(e) => e.to_string().hash(state),
             Self::BuildRequestClient(e) => e.to_string().hash(state),
+            Self::CreateRequestItem(uri, s, e) => (uri, s, e.to_string()).hash(state),
             Self::BuildGithubClient(e) => e.to_string().hash(state),
             Self::GithubRequest(e) => e.to_string().hash(state),
             Self::InvalidGithubUrl(s) => s.hash(state),
