b6313e9810d3f00dc3eb0e871ea08c0168c0df0f
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-10-22 17:42:09 +0200

    Fix benches & remove TryFrom which was only used in bench

--- lychee-lib/src/types/input/content.rs
+++ lychee-lib/src/types/input/content.rs
@@ -4,11 +4,8 @@
 //! input sources, along with metadata about the source and file type.
 
 use super::source::ResolvedInputSource;
-use crate::ErrorKind;
 use crate::types::FileType;
 use std::borrow::Cow;
-use std::fs;
-use std::path::PathBuf;
 
 /// Encapsulates the content for a given input
 #[derive(Debug)]
@@ -41,29 +38,5 @@ impl InputContent {
             file_type,
             content: cow.into_owned(),
         }
-    }
-}
-
-impl TryFrom<&PathBuf> for InputContent {
-    type Error = crate::ErrorKind;
-
-    fn try_from(path: &PathBuf) -> std::result::Result<Self, Self::Error> {
-        let input = match fs::read_to_string(path) {
-            Ok(content) => content,
-            Err(e) if e.kind() == std::io::ErrorKind::InvalidData => {
-                log::warn!(
-                    "Skipping file with invalid UTF-8 content: {}",
-                    path.display()
-                );
-                return Err(ErrorKind::ReadFileInput(e, path.clone()));
-            }
-            Err(e) => return Err(ErrorKind::ReadFileInput(e, path.clone())),
-        };
-
-        Ok(Self {
-            source: ResolvedInputSource::String(Cow::Owned(input.clone())),
-            file_type: FileType::from(path),
-            content: input,
-        })
     }
 }
