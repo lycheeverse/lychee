70f793b5f74c3563d8a9c0eb8d8c987577d9c546
rina <k@rina.fyi>
2025-11-16 16:23:05 +1000

    move request_error into separate file.

--- lychee-lib/src/types/request.rs
+++ lychee-lib/src/types/request.rs
@@ -1,76 +1,7 @@
 use std::{borrow::Cow, convert::TryFrom, fmt::Display};
-use thiserror::Error;
 
-use crate::{BasicAuthCredentials, ErrorKind, RawUri, Response, Status, Uri};
-use crate::{InputSource, ResolvedInputSource};
-
-/// An error which occurs while trying to construct a [`Request`] object.
-/// That is, an error which happens while trying to load links from an input
-/// source.
-#[derive(Error, Debug, PartialEq, Eq, Hash)]
-pub enum RequestError {
-    /// Unable to construct a URL for a link appearing within the given source.
-    #[error("Error building URL for {0}: {2}")]
-    CreateRequestItem(RawUri, ResolvedInputSource, #[source] Box<ErrorKind>),
-
-    /// Unable to load the content of an input source.
-    #[error("Error reading input '{0}': {1}")]
-    GetInputContent(InputSource, #[source] Box<ErrorKind>),
-
-    /// Unable to load an input source directly specified by the user.
-    #[error("Error reading user input '{0}': {1}")]
-    UserInputContent(InputSource, #[source] Box<ErrorKind>),
-}
-
-impl RequestError {
-    /// Get the underlying cause of this [`RequestError`].
-    #[must_use]
-    pub const fn error(&self) -> &ErrorKind {
-        match self {
-            Self::CreateRequestItem(_, _, e)
-            | Self::GetInputContent(_, e)
-            | Self::UserInputContent(_, e) => e,
-        }
-    }
-
-    /// Convert this [`RequestError`] into its source error.
-    #[must_use]
-    pub fn into_error(self) -> ErrorKind {
-        match self {
-            Self::CreateRequestItem(_, _, e)
-            | Self::GetInputContent(_, e)
-            | Self::UserInputContent(_, e) => *e,
-        }
-    }
-
-    /// Get (a clone of) the input source within which the error happened.
-    #[must_use]
-    pub fn input_source(&self) -> InputSource {
-        match self {
-            Self::CreateRequestItem(_, src, _) => src.clone().into(),
-            Self::GetInputContent(src, _) | Self::UserInputContent(src, _) => src.clone(),
-        }
-    }
-
-    /// Convert this request error into a failure [`Response`] for reporting
-    /// purposes. However, if this error was caused by failing to load a
-    /// user-specified input, then an Err is returned for propagating back
-    /// to the user.
-    #[must_use]
-    pub fn into_response(self) -> Result<Response, ErrorKind> {
-        match self {
-            RequestError::UserInputContent(_, e) => Err(*e),
-            e => {
-                let src = e.input_source();
-                Ok(Response::new(
-                    Uri::try_from("error:").unwrap(),
-                    Status::RequestError(e),
-                    src,
-                ))
-            }
-        }
-    }
-}
+use crate::ResolvedInputSource;
+use crate::{BasicAuthCredentials, ErrorKind, Uri};
 
 /// A request type that can be handle by lychee
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
