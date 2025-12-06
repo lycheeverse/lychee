3a42e9fc0205250c99729799da4c3b9d751414a4
JayJayArr <JayArr@outlook.de>
2025-12-06 11:04:10 +0100

    refactor: move WikiLink cleaning to WikiLink Module

--- lychee-lib/src/checker/wikilink/mod.rs
+++ lychee-lib/src/checker/wikilink/mod.rs
@@ -1,6 +1,78 @@
 //! `WikiLink` Module
 //!
 //! This module contains a Indexer and a Resolver for `WikiLinks`
+//! The Indexer recursively indexes the subdirectories and files in a given base-directory mapping
+//! the filename to the full path
+//! The Resolver looks for found `WikiLinks` in the Index thus resolving the `WikiLink` to a full
+//! filepath
 
 pub(crate) mod index;
 pub(crate) mod resolver;
+
+use crate::ErrorKind;
+use pulldown_cmark::CowStr;
+
+/// In Markdown Links both '#' and '|' act as modifiers
+/// '#' links to a specific Header in a file
+/// '|' is used to modify the link name, a so called "pothole"
+const MARKDOWN_FRAGMENT_MARKER: char = '#';
+const MARKDOWN_POTHOLE_MARKER: char = '|';
+
+/// Clean a `WikiLink` by removing potholes and fragments from a `&str`
+pub(crate) fn wikilink(input: &str, has_pothole: bool) -> Result<CowStr<'_>, ErrorKind> {
+    // Strip potholes (|) from wikilinks
+    let mut stripped_input = if has_pothole {
+        pulldown_cmark::CowStr::Borrowed(
+            &input[0..input.find(MARKDOWN_POTHOLE_MARKER).unwrap_or(input.len())],
+        )
+    } else {
+        CowStr::Borrowed(input)
+    };
+
+    // Strip fragments (#) from wikilinks, according to the obsidian spec
+    // fragments always come before potholes
+    // https://help.obsidian.md/links#Change+the+link+display+text
+    if stripped_input.contains(MARKDOWN_FRAGMENT_MARKER) {
+        stripped_input = pulldown_cmark::CowStr::Borrowed(
+            // In theory a second '#' could be inserted into the pothole, so searching for the
+            // first occurence from the left should yield the correct result
+            &input[0..input.find(MARKDOWN_FRAGMENT_MARKER).unwrap_or(input.len())],
+        );
+    }
+    if stripped_input.is_empty() {
+        return Err(ErrorKind::EmptyUrl);
+    }
+    Ok(stripped_input)
+}
+
+#[cfg(test)]
+mod tests {
+    use pulldown_cmark::CowStr;
+    use rstest::rstest;
+
+    use crate::checker::wikilink::wikilink;
+
+    #[rstest]
+    #[case("|foo", true)]
+    #[case("|foo#bar", true)]
+    #[case("#baz", false)]
+    fn test_empty_wikilinks_yield_no_errors(#[case] input: &str, #[case] has_pothole: bool) {
+        let result = wikilink(input, has_pothole);
+        assert!(result.is_err());
+    }
+
+    #[rstest]
+    #[case("foo|bar", true, "foo")]
+    #[case("foo#bar", true, "foo")]
+    #[case("foo#bar|baz", false, "foo")]
+    #[case("foo#bar|baz#hashtag_in_pothole", false, "foo")]
+    fn test_fragment_and_pothole_removal(
+        #[case] input: &str,
+        #[case] has_pothole: bool,
+        #[case] actual: &str,
+    ) {
+        let result = wikilink(input, has_pothole).unwrap();
+        let actual = CowStr::Borrowed(actual);
+        assert_eq!(result, actual);
+    }
+}
