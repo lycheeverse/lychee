7c93c8b9d5d7482cb19cc318c619f51e9656eda4
rina <k@rina.fyi>
2026-01-06 13:45:51 +1000

    stash

--- lychee-lib/src/types/base.rs
+++ lychee-lib/src/types/base.rs
@@ -6,28 +6,25 @@ use crate::{ErrorKind, ResolvedInputSource};
 
 /// When encountering links without a full domain in a document,
 /// the base determines where this resource can be found.
-/// Both, local and remote targets are supported.
+/// Both, local and remote targets are supported. Local paths
+/// are represented as file:// URLs.
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Clone)]
 #[allow(variant_size_differences)]
 #[serde(try_from = "String")]
-pub enum Base {
-    /// Local file path pointing to root directory
-    Local(PathBuf),
-    /// Remote URL pointing to a website homepage
-    Remote(Url),
+pub struct Base {
+    base_url: Url
 }
 
 impl Base {
+
+    pub fn new(base_url: Url) -> Base {
+
+    }
+
     /// Join link with base url
     #[must_use]
     pub(crate) fn join(&self, link: &str) -> Option<Url> {
-        match self {
-            Self::Remote(url) => url.join(link).ok(),
-            Self::Local(path) => {
-                let full_path = path.join(link);
-                Url::from_file_path(full_path).ok()
-            }
-        }
+        self.base_url.join(link).ok()
     }
 
     pub(crate) fn from_source(source: &ResolvedInputSource) -> Option<Base> {
