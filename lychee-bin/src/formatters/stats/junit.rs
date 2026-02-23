02cd99590394dfd884b0d64b4725771867dce9d6
Thomas Zahner <thomas.zahner@protonmail.ch>
2026-02-23 14:38:07 +0100

    Use quick_junit

--- lychee-bin/src/formatters/stats/junit.rs
+++ lychee-bin/src/formatters/stats/junit.rs
@@ -1,7 +1,8 @@
 use std::collections::{HashMap, HashSet};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use lychee_lib::{InputSource, ResponseBody};
+use quick_junit::{NonSuccessKind, Report, TestCase, TestCaseStatus, TestSuite};
 
 use super::StatsFormatter;
 use crate::formatters::stats::{OutputStats, ResponseStats};
@@ -20,96 +21,138 @@ impl Junit {
 impl StatsFormatter for Junit {
     /// Format stats as JSON object
     fn format(&self, stats: OutputStats) -> Result<String> {
-        Ok(junit_xml(stats.response_stats))
+        junit_xml(stats.response_stats)
+            .to_string()
+            .context("Unable to convert JUnit report to XML")
     }
 }
 
 /// Unfortunately there is no official specification of this format,
 /// but there is documentation available at <https://github.com/testmoapp/junitxml>.
 /// Note that using a library would be overkill in this case.
-fn junit_xml(stats: ResponseStats) -> String {
-    let testcases = junit_testcases(stats);
-    format!(
-        r#"<?xml version="1.0" encoding="UTF-8"?>
-<testsuites><testsuite name="lychee link check results">{testcases}</testsuite></testsuites>"#
-    )
+fn junit_xml(stats: ResponseStats) -> Report {
+    const NAME: &str = "lychee link check results";
+    let mut report = Report::new(NAME);
+
+    let mut test_suite = TestSuite::new(NAME);
+    test_suite.add_test_cases(junit_testcases(stats));
+
+    report.add_test_suite(test_suite);
+    report
 }
 
-fn junit_testcases(stats: ResponseStats) -> String {
-    let failures = junit_testcases_group(stats.error_map, Some("failure"), "Failed");
-    let skipped = junit_testcases_group(stats.excluded_map, Some("skipped"), "Excluded");
-    let sucesses = junit_testcases_group(stats.success_map, None, "Successful");
-    let redirected = junit_testcases_group(stats.redirect_map, None, "Redirected");
+fn junit_testcases(stats: ResponseStats) -> Vec<TestCase> {
+    let failures = junit_testcases_group(
+        stats.error_map,
+        TestCaseStatus::non_success(NonSuccessKind::Failure),
+        "Failed",
+    );
+    let skipped = junit_testcases_group(stats.excluded_map, TestCaseStatus::skipped(), "Excluded");
+    let sucesses =
+        junit_testcases_group(stats.success_map, TestCaseStatus::success(), "Successful");
+    let redirected =
+        junit_testcases_group(stats.redirect_map, TestCaseStatus::success(), "Redirected");
 
-    format!("{failures}\n\n{skipped}\n\n{sucesses}\n\n{redirected}\n")
+    [failures, skipped, sucesses, redirected].concat()
 }
 
 fn junit_testcases_group(
     map: HashMap<InputSource, HashSet<ResponseBody>>,
-    tag: Option<&str>,
+    status: TestCaseStatus,
     reason: &str,
-) -> String {
-    if map.is_empty() {
-        return "".into();
-    }
-
-    let xml = map
-        .into_iter()
-        .flat_map(|(source, b)| {
+) -> Vec<TestCase> {
+    map.into_iter()
+        .flat_map(move |(source, b)| {
+            let status = status.clone();
             b.into_iter().map(move |response| {
-                let file = xml_property("file", &source);
-                let name = xml_property("name", format!("{reason} {}", response.uri));
-                let line = response
-                    .span
-                    .map(|s| xml_property("line", s.line))
-                    .unwrap_or_default();
+                let name = format!("{reason} {}", response.uri);
+                let mut testcase = TestCase::new(name, status.clone());
 
-                let message = xml_property("message", &response);
+                testcase
+                    .extra
+                    .insert("file".into(), source.to_string().into());
 
-                let inner = tag
-                    .map(|tag| format!("\n        <{tag} {message} />",))
-                    .unwrap_or_default();
+                if let Some(span) = response.span {
+                    testcase
+                        .extra
+                        .insert("line".into(), span.line.to_string().into());
+                }
 
-                format!(
-                    r#"
-    <testcase {name} {file} {line}>
-        <system-out>{response}</system-out>{inner}
-    </testcase>"#
-                )
+                testcase.set_system_out(response.to_string());
+                testcase.status.set_message(response.to_string());
+
+                testcase
             })
         })
-        .collect::<Vec<String>>()
-        .join("\n");
-
-    let comment = format!("<!-- {reason} -->");
-    format!("\n    {comment}\n{xml}")
-}
-
-/// Write an XML property `key="value"` where the contents
-/// of `value` is escaped.
-fn xml_property<S: ToString>(key: &str, value: S) -> String {
-    let value = value.to_string().replace('"', "&quot;"); // only need to escape `"`
-    format!(r#"{key}="{value}""#)
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::formatters::stats::{StatsFormatter, get_dummy_stats, junit::Junit};
+    use std::collections::{HashMap, HashSet};
+
+    use http::StatusCode;
+    use lychee_lib::{InputSource, ResponseBody, Status};
+    use url::Url;
+
+    use crate::formatters::stats::{OutputStats, StatsFormatter, get_dummy_stats, junit::Junit};
 
     #[test]
     fn test_junit_formatter() {
         let formatter = Junit::new();
-        let result = formatter.format(get_dummy_stats()).unwrap();
+        let result = formatter.format(stats()).unwrap();
 
         assert_eq!(
             result,
             r#"<?xml version="1.0" encoding="UTF-8"?>
-<testsuites><testsuite name="lychee link check results">
-    <testcase name="Broken URI https://github.com/mre/idiomatic-rust-doesnt-exist-man" file="https://example.com/">
-        <failure message="https://github.com/mre/idiomatic-rust-doesnt-exist-man | 404 Not Found: Not Found">Not Found</failure>
-    </testcase>
-</testsuite></testsuites>
+<testsuites name="lychee link check results" tests="4" failures="1" errors="0">
+    <testsuite name="lychee link check results" tests="4" disabled="1" errors="0" failures="1">
+        <testcase name="Failed https://github.com/mre/idiomatic-rust-doesnt-exist-man" file="https://example.com/">
+            <failure message="https://github.com/mre/idiomatic-rust-doesnt-exist-man | 404 Not Found: Not Found"/>
+            <system-out>https://github.com/mre/idiomatic-rust-doesnt-exist-man | 404 Not Found: Not Found</system-out>
+        </testcase>
+        <testcase name="Excluded https://excluded.org/" file="https://example.com/">
+            <skipped message="https://excluded.org/"/>
+            <system-out>https://excluded.org/</system-out>
+        </testcase>
+        <testcase name="Successful https://success.org/" file="https://example.com/">
+            <system-out>https://success.org/</system-out>
+        </testcase>
+        <testcase name="Redirected https://redirected.dev/" file="https://example.com/">
+            <system-out>https://redirected.dev/ | Redirect: Followed 2 redirects resolving to the final status of: OK. Redirects: https://1.dev/ --[308]--&gt; https://2.dev/ --[308]--&gt; http://redirected.dev/</system-out>
+        </testcase>
+    </testsuite>
+</testsuites>
 "#
         );
+    }
+
+    fn stats() -> OutputStats {
+        let mut stats = get_dummy_stats();
+        stats.response_stats.total += 2;
+        stats.response_stats.successful += 1;
+        stats.response_stats.excludes += 1;
+
+        let source = InputSource::RemoteUrl(Box::new(Url::parse("https://example.com").unwrap()));
+
+        stats.response_stats.success_map = HashMap::from([(
+            source.clone(),
+            HashSet::from([ResponseBody {
+                uri: "https://success.org".try_into().unwrap(),
+                status: Status::Ok(StatusCode::OK),
+                span: None,
+            }]),
+        )]);
+
+        stats.response_stats.excluded_map = HashMap::from([(
+            source.clone(),
+            HashSet::from([ResponseBody {
+                uri: "https://excluded.org".try_into().unwrap(),
+                status: Status::Excluded,
+                span: None,
+            }]),
+        )]);
+
+        stats
     }
 }
