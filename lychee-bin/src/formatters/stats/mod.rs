cb5baed74cd268536da9a645e9b04ffd4ccb38da
Cristian Klein <cristian.klein@elastisys.com>
2026-02-28 19:21:03 +0100

    Make clippy happy

--- lychee-bin/src/formatters/stats/mod.rs
+++ lychee-bin/src/formatters/stats/mod.rs
@@ -83,10 +83,10 @@ where
             if let Some(responses) = first.get(source) {
                 sorted_responses.extend(responses.iter());
             }
-            if let Some(second_map) = second {
-                if let Some(responses) = second_map.get(source) {
-                    sorted_responses.extend(responses.iter());
-                }
+            if let Some(second_map) = second
+                && let Some(responses) = second_map.get(source)
+            {
+                sorted_responses.extend(responses.iter());
             }
             sorted_responses.sort_by(|a, b| {
                 let (a, b) = (a.to_string().to_lowercase(), b.to_string().to_lowercase());
@@ -185,7 +185,7 @@ fn get_dummy_stats() -> OutputStats {
         success_map: HashMap::default(),
         error_map,
         excluded_map: HashMap::default(),
-        timeout_map: timeout_map,
+        timeout_map,
         detailed_stats: true,
     };
 
