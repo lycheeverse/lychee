885a6544bd8778f4eab9a15b0a52a658e88bcd36
rina <k@rina.fyi>
2026-06-20 13:43:35 +1000

    py

---
+++ fixtures/fragments/make-special-casing.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+"""
+Pipe in the contents of https://www.unicode.org/Public/UNIDATA/SpecialCasing.txt
+"""
+
+a = sys.stdin.read()
+
+# Turn contiguous regions beginning with '#' into Markdown code blocks
+a = re.sub(r'^#[^\n]*\n\n', lambda x: x[0].rstrip() + '\n```\n\n', a, flags=re.MULTILINE)
+a = re.sub(r'\n\n#', '\n\n```\n#', a)
+
+# Turn lines not beginning with backtick or # into Markdown headings.
+a = re.sub(r'^[^`#\n]', lambda x: '# ' + x[0], a, flags=re.MULTILINE)
+
+# Turn Unicode codepoints (4 hex digits) into HTML entities and make the
+# codepoints adjacent to trigger special casing.
+a = re.sub(r'[0-9A-F]{4}', lambda x: '&#x' + x[0] + ';', a)
+a = re.sub(r'; &', ';&', a)
+
+print('```')
+print(a)
+
